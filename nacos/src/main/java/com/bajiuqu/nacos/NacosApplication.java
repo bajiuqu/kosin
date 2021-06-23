5cad0972519d9ab8bffdb27f08ee9c9bfd3ab366
Ruimin Wang <ruiminwang@163.com>
2021-06-23 18:37:35 +0800

    配置服务注册中心

--- nacos/src/main/java/com/bajiuqu/nacos/NacosApplication.java
+++ nacos/src/main/java/com/bajiuqu/nacos/NacosApplication.java
@@ -1,5 +1,6 @@
 package com.bajiuqu.nacos;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
