2b85542ea4567a68206359006636c0900a480bf9
Ruimin Wang <ruiminwang@163.com>
2021-06-24 08:56:32 +0800

    nacos-server 配置日志

--- nacos/src/main/java/com/bajiuqu/nacos/NacosApplication.java
+++ nacos/src/main/java/com/bajiuqu/nacos/NacosApplication.java
@@ -1,14 +1,19 @@
 package com.bajiuqu.nacos;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
+@NacosPropertySource(dataId = "example", autoRefreshed = true)
 public class NacosApplication {
 
     public static void main(String[] args) {
+        log.info("nacos-server 开始启动");
         SpringApplication.run(NacosApplication.class, args);
+        log.info("nacos-server 启动完成");
     }
 
 }
