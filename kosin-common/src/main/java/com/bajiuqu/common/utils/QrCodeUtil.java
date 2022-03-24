1ea7b7bcf25b1cbfe7bc67e2df5bec98e054b33c
wangruimin <ruiminwang@163.com>
2022-03-24 19:34:14 +0800

    修改BUG

---
+++ kosin-common/src/main/java/com/bajiuqu/common/utils/QrCodeUtil.java
@@ -0,0 +1,15 @@
+package com.bajiuqu.common.utils;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author 小艺小艺
+ * @date 2022-02-09 16:54:45
+ * @Description 二维码工具类
+ */
+
+@Slf4j
+public class QrCodeUtil {
+
+
+}
