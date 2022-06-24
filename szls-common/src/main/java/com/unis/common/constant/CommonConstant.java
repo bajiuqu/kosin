4d0e1776728e603e5caef91ff297f1b3cfb81576
wangrm <ruiminwang@163.com>
2022-06-24 17:53:10 +0800

    添加模块，修改项目结构

---
+++ szls-common/src/main/java/com/unis/common/constant/CommonConstant.java
@@ -0,0 +1,34 @@
+package com.unis.common.constant;
+
+public class CommonConstant {
+
+    public static final String ZERO_STR = "0";
+    public static final String DEFAULT_PASSWD = "Lscbj@12345";
+    public static final String SUCCESS = "成功";
+    public static final String FAIL = "失败";
+    public static final String EXCEPTION = "出现异常";
+    public static final String NEW = "new";
+    public static final String EDIT = "edit";
+    public static final String ADMIN_RIGHTS_REDIS_KEY_PRE = "admin_rights";
+    public static final String USER_RIGHTS_REDIS_KEY_PRE = "user_rights";
+    public static final String COMMA = ",";
+    public static final String SYSTEM_LOG = "system_Log";
+    public static final String PAGE_ACCESS_LOG = "page_Access_Log";
+    public static final String SYS_LOG_LOGIN = "sys_Log_Login";
+    public static final String ADD_CODE = "10320001";
+    public static final String DELETE_CODE = "10320002";
+    public static final String UPDATE_CODE = "10320003";
+    public static final String QUERY_CODE = "10320004";
+    public static final String INFO_CODE = "10220001";
+    public static final String WARNING_CODE = "10220002";
+    public static final String ERROR_CODE = "10220003";
+    public static final String CRITICAL_CODE = "10220004";
+    public static final String EXCEPTION_CODE = "102200005";
+    public static final String DEBUG_CODE = "10220006";
+    public static final String STRING_UNDEFINED = "undefined";
+    public static final String STRING_NULL = "null";
+    public static final int NO_RESULT = 0;
+
+    public CommonConstant() {
+    }
+}
